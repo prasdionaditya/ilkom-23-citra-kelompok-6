4ea4306d68bfc4092b9ea380e4aeba813db1897c
prasdionaditya <prasdionaditya@gmail.com>
2025-04-20 12:25:16 +0800

    tambah elemen beforeWrapper di fungsi setupSlider untuk pengaturan slider gambar

--- app/static/JavaScript/script.js
+++ app/static/JavaScript/script.js
@@ -1,3 +1,4 @@
 // Fungsi untuk halaman index.html - Slider gambar
 function setupSlider() {
     const sliderHandle = document.getElementById("sliderHandle");
+    const beforeWrapper = document.querySelector(".img-before-wrapper");
